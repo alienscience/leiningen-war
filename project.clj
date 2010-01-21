9994548ee904141873d86c6840169de269acd9d3
Alien Science <saul@alienscience.org.uk>
2010-01-21 16:42:58 +0000

    Added to clojars.org

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject leiningen-war "0.0.1"
+(defproject uk.org.alienscience/leiningen-war "0.0.1"
   :description "Leiningen war plugin."
   :namespaces [leiningen.war leiningen.uberwar]
   :dependencies []
-  :dev-dependencies [])
+  :dev-dependencies [[org.clojure/clojure "1.1.0"]
+                     [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]])
 
