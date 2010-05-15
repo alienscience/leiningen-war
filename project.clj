fcd2345482d577ce242b8408e7c5d9f02076c3fc
Punyashloka Biswal <punya.biswal@gmail.com>
2010-05-15 14:23:57 -0700

    Use clojure-contrib 1.1.0 release, now that it's out

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :namespaces [leiningen.war leiningen.uberwar]
   :dependencies []
   :dev-dependencies [[org.clojure/clojure "1.1.0"]
-                     [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]])
+                     [org.clojure/clojure-contrib "1.1.0"]])
 
