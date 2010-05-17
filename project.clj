ace0031c1b6134a2337d08b57b9d868cb03ddd85
Alien Science <saul@alienscience.org.uk>
2010-05-17 21:00:32 +0100

    Release 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.org.alienscience/leiningen-war "0.0.2"
+(defproject uk.org.alienscience/leiningen-war "0.0.3"
   :description "Leiningen war plugin."
   :namespaces [leiningen.war leiningen.uberwar]
   :dependencies []
