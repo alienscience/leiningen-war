df6eb84b0abe4fa8542b82da106e1cce556249b1
Alien Science <saul@alienscience.org.uk>
2010-08-10 10:27:48 +0100

    Release 0.0.7

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject uk.org.alienscience/leiningen-war "0.0.6"
+(defproject uk.org.alienscience/leiningen-war "0.0.7"
   :description "Leiningen war plugin."
-  :namespaces [leiningen.war leiningen.uberwar]
   :dependencies []
   :dev-dependencies [[org.clojure/clojure "1.1.0"]
                      [org.clojure/clojure-contrib "1.1.0"]
